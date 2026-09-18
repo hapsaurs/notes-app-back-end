262a33b203d56b4b18706614cdc794a2ab2053ec
Hafshah <hafshahazzahra2105@gmail.com>
2026-09-18 14:56:48 +0700

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import daStyle from 'eslint-config-dicodingacademy';
+
+
+export default [
+  daStyle,
+  { files: ['**/*.js'], languageOptions: { sourceType: 'module' } },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+];
+console.log('Hallo kita akan membuat RESTful API');
